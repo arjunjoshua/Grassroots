9ce70306487964445f667c1382de2e95ce2756ab
arjunjoshua <arjunjoshua@gmail.com>
2023-07-22 15:31:32 +0100

    Added registerScreen and improved login logic.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/loginScreen';
 import HomeScreen from './screens/homeScreen';
+import RegisterScreen from './screens/registerScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Log In' }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Register' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/registerScreen.js
+++ screens/registerScreen.js
@@ -2,43 +2,47 @@ import React, { useState } from 'react';
 import { Button, TextInput, View, Alert, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 
-const LoginScreen = ({ navigation }) => {
+const RegisterScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
   const validateForm = () => {
     if (username.trim() === '' || password.trim() === '') {
-      Alert.alert('Validation Error', 'Please enter username and password');
+      Alert.alert('Validation Error', 'Username and password are required');
       return false;
     }
 
     return true;
   };
 
-  const handleLogin = async () => {
+  const handleRegister = async () => {
+    if (!validateForm()) {
+      return;
+    }
+
     setLoading(true);
 
     const user = {
-      username: username,
-      password: password,
+      username,
+      password,
     };
 
     axios
-      .post('http://139.184.223.176:5000/api/auth/login', user)
+      .post('http://139.184.223.176:5000/api/register', user)
       .then((response) => {
         setLoading(false);
         if (response.data.status === 'success') {
-          // save token, navigate to home or do something
-          alert('Logged in successfully');
+          Alert.alert('Success', 'Registered successfully');
+          navigation.navigate('Login');
         } else {
-          alert('Invalid username or password');
+          Alert.alert('Error', 'Something went wrong!');
         }
       })
       .catch((error) => {
         setLoading(false);
         console.error('There was an error!', error);
-        alert('Something went wrong! Please try again later.');
+        Alert.alert('Error', 'Something went wrong! Please try again later.');
       });
   };
 
@@ -55,14 +59,10 @@ const LoginScreen = ({ navigation }) => {
         placeholder="Password"
         secureTextEntry
       />
-      <Button title="Log in" onPress={handleLogin} disabled={loading} />
+      <Button title="Register" onPress={handleRegister} disabled={loading} />
       {loading && <ActivityIndicator />}
-      <Button
-        title="Don't have an account? Sign up"
-        onPress={() => navigation.navigate('Register')}
-      />
     </View>
   );
 };
 
-export default LoginScreen;
+export default RegisterScreen;
