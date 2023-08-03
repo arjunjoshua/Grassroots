d739568931649519122070219147b947a149b6c5
arjunjoshua <arjunjoshua@gmail.com>
2023-08-03 14:43:06 +0100

    Added notifications to homeScreen.js

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
-import { View, Text, Button, Alert, StyleSheet, FlatList } from 'react-native';
+import { View, Text, Button, Alert, StyleSheet, FlatList, Modal, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import { Badge, IconButton } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { IP_ADDRESS } from '../constants/constants';
+import { set } from 'mongoose';
 
 const HomeScreen = ({ route, navigation }) => {
   const { token, userID } = route.params;
   const [teams, setTeams] = useState([]);
-
-  // const handleCreateTeam = () => {
-  //   const token = route.params.token;
-  //   navigation.navigate('CreateTeam', { token,  userID });
-  // };
+  const [notifications, setNotifications] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     axios.get(`${IP_ADDRESS}:5000/api/teamsInfo`, { params: { userID } }) 
@@ -19,9 +19,25 @@ const HomeScreen = ({ route, navigation }) => {
       })
       .catch((error) => {
         console.error('There was an error!', error);
-        Alert.alert('Error', 'Something went wrong! Please try again later.');
+        Alert.alert('Error', 'Error retrieving teams. Please try again later.');
+      });
+
+    axios.get(`${IP_ADDRESS}:5000/api/notifications`, { params: {userID} })
+      .then((response) => {
+        setNotifications(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+        Alert.alert('Error', 'Error retrieving notifications. Please try again later.');
       });
   }, []);
+
+  const renderNotification = ({ item }) => (
+    <View style={styles.notificationContainer}>
+      <Text style={styles.notificationMessage}>{item.message}</Text>
+      <Text style={styles.notificationDate}>{new Date(item.date).toLocaleString()}</Text>
+    </View>
+  );
 
   const renderTeam = ({ item }) => (
     <View style={styles.teamContainer}>
@@ -33,17 +49,51 @@ const HomeScreen = ({ route, navigation }) => {
   
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Your Teams: </Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Your Teams: </Text>
+        <IconButton
+          icon={() => <Badge size={10} style={styles.badge}>{notifications.length}</Badge>}
+          color="#000"
+          size={20}
+          onPress={() => setModalVisible(true)}
+        />
+      </View>
+
       <FlatList
         data={teams}
         renderItem={renderTeam}
         keyExtractor={(item) => item._id}
       />
-      {/* <Button title="Create Team" onPress={handleCreateTeam} style={styles.button} /> */}
+
+<Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={ () => { 
+          setModalVisible(!modalVisible); 
+        } }
+      >
+        <View style={styles.modalView}>
+          <TouchableOpacity 
+          onPress={ () => { setModalVisible(!modalVisible); } }
+             style={styles.closeButton}>
+            <MaterialCommunityIcons name="close" color="#333" size={24} />
+          </TouchableOpacity>
+          {notifications.length === 0 ? (
+            <Text style={styles.modalText}>You have no new notifications</Text>
+          ) : (
+            <FlatList
+              data={notifications}
+              renderItem={({ item }) => <Text style={styles.modalText}>{item.message}</Text>}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          )}
+        </View>
+      </Modal>
     </View>
   );
 };
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -72,27 +122,75 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  button: {
-    marginTop: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    backgroundColor: '#2196f3',
-    borderRadius: 5,
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
   },
-
   team_name: {
     fontSize: 17,
     fontWeight: 'bold',
   },
-
   team_info: {
     fontSize: 15,
-  }
+  },
+  badge: {
+    position: 'absolute',
+    right: -6,
+    top: -3,
+    backgroundColor: 'red',
+    zIndex: 2,
+  },
+  notificationContainer: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  notificationMessage: {
+    fontSize: 16,
+  },
+  notificationDate: {
+    fontSize: 14,
+    color: '#888',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    marginTop: 100,
+    width: 400,
+    backgroundColor: "grey",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
 });
+
 
 export default HomeScreen;
