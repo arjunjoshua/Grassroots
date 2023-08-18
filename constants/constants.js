c2855b31593eabb56f4ca2b17b103f2886430682
arjunjoshua <arjunjoshua@gmail.com>
2023-08-18 01:25:56 +0100

    Added port number to local debugging host

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-//export const IP_ADDRESS = 'http://10.2.240.103';  //Eduroam
+//export const IP_ADDRESS = 'http://10.2.240.103:3000';  //Eduroam
 //export const IP_ADDRESS = 'http://10.108.213.36';  //Pret
 export const IP_ADDRESS = 'https://grassroots-backend.vercel.app'; //Vercel hosting (production)
 export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
